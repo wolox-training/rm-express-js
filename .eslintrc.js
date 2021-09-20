649cb2c307866341bc0d618c1bad738e95efe2a4
ricardoamoreno <ricardo.a.moreno@wolox.co>
2021-09-20 15:32:53 -0500

    Add no console rule on eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,7 @@
-module.exports = require('eslint-config-wolox-node');
+//module.exports = require('eslint-config-wolox-node');
+module.exports = {
+  extends: 'eslint-config-wolox-node',
+  rules: {
+    'no-console': 'off'
+  }
+};
