adcaaaabf33c23ae71ffdcd906ac64ff4c4763c7
ricardoamoreno <ricardo.a.moreno@wolox.co>
2021-09-20 15:50:55 -0500

    fix quote-garden url on weets service

--- app/services/weets.js
+++ app/services/weets.js
@@ -4,7 +4,7 @@ const { externalApiError, notFoundError } = require('../errors');
 
 exports.getWeet = () =>
   axios
-    .get('https://quote-garden.herokuapp.com/api/v3/quotes/rando')
+    .get('https://quote-garden.herokuapp.com/api/v3/quotes/random')
     .then(response => response.data.data[0])
     .catch(error => {
       if (error.response && error.response.status === 404) {
