3d46c0af6a5194679b00dd9bec008b4f2e42a3b8
ricardoamoreno <ricardo.a.moreno@wolox.co>
2021-09-22 14:02:50 -0500

    add ajv user validator

--- app/routes.js
+++ app/routes.js
@@ -1,11 +1,9 @@
-// const controller = require('./controllers/controller');
 const { healthCheck } = require('./controllers/healthCheck');
 const { signUp } = require('./controllers/users');
+const validateDto = require('../app/middlewares/validate-dto');
+const userSchema = require('../app/helpers/ajv-schemas/user');
 
 exports.init = app => {
   app.get('/health', healthCheck);
-  app.post('/users', signUp);
-  // app.get('/endpoint/get/path', [], controller.methodGET);
-  // app.put('/endpoint/put/path', [], controller.methodPUT);
-  // app.post('/endpoint/post/path', [], controller.methodPOST);
+  app.post('/users', validateDto(userSchema), signUp);
 };
