9d009e19a663cdd151627ed8a9b754adb2ed97d1
ricardoamoreno <ricardo.a.moreno@wolox.co>
2021-10-04 10:02:52 -0500

    update and rename weetItneractor to createWeetInteractor

--- app/interactors/weets.js
+++ app/interactors/weets.js
@@ -3,11 +3,11 @@ const logger = require('../logger');
 const { externalApiError, validationError } = require('../errors');
 const { externalApiErrorMessage, weetErrorMessage } = require('../helpers/constants');
 
-exports.weetsInteractor = async (req, res, next) => {
-  const userId = req.user.id;
+exports.createWeetInteractor = async user => {
+  const userId = user.id;
   const weet = await getWeet();
-  if (!weet) return next(externalApiError(externalApiErrorMessage));
-  if (weet.quoteText.length > 140) return next(validationError(weetErrorMessage));
+  if (!weet) throw externalApiError(externalApiErrorMessage);
+  if (weet.quoteText.length > 140) throw validationError(weetErrorMessage);
   const content = weet.quoteText;
   const weetResp = await createWeet({ userId, content });
   logger.info(`weet ${weetResp.id} was created succesfully`);
