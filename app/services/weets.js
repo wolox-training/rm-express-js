32f84192f0a949856ba6c85e5c5d57cb2637a325
ricardoamoreno <ricardo.a.moreno@wolox.co>
2021-10-04 12:55:15 -0500

    add status code constant in weets service

--- app/services/weets.js
+++ app/services/weets.js
@@ -1,6 +1,8 @@
 const axios = require('axios');
 const { Weet } = require('../models');
 const logger = require('../logger');
+const errors = require('../errors');
+const { statusCodes } = require('../middlewares/errors');
 
 const { randomWeetEndpoint } = require('../../config').common.weetApi;
 const { externalApiError, notFoundError, databaseError } = require('../errors');
@@ -16,7 +18,7 @@ exports.getWeet = async () => {
     return response.data.data[0];
   } catch (error) {
     logger.error(error.message);
-    if (error.response && error.response.status === 404) {
+    if (error.response && error.response.status === statusCodes[errors.NOT_FOUND_ERROR]) {
       throw notFoundError(weetNotFoundErrorMessage);
     }
     throw externalApiError(externalApiErrorMessage);
